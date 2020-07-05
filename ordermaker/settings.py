bcb40908f2affed2500a476bc38fed2cf014bf28
Francisco Javier Lendínez Tirado <fjlendinez@talenthackers.net>
2020-07-05 11:26:23 +0200

    Make async requests Added db sync_to_async from channels Added secret_key from env

--- ordermaker/settings.py
+++ ordermaker/settings.py
@@ -8,7 +8,7 @@ from pydantic import AnyHttpUrl
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@l0@_tof05-op)56&zrt#92h=1#m16@p64a_&-85c=l+#r6hhl'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'Bruce Wayne is Batman!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- products/views.py
+++ products/views.py
@@ -2,19 +2,31 @@ from typing import List
 
 from fastapi import APIRouter
 
+from ordermaker.db_sync import database_sync_to_async
 from products.models import ProductModel
 from products.schemas import Product, ProductIn
 
 router = APIRouter()
 
 
+@database_sync_to_async
+def get_all():
+    return list(ProductModel.objects.all())
+
+
 @router.get("/", response_model=List[Product])
-def list_products():
-    products = list(ProductModel.objects.all())
+async def list_products():
+    products = await get_all()
     return products
 
-@router.post("/", response_model=Product)
-def create_product(product_data: ProductIn):
-    product_in_db = ProductModel.objects.create(**dict(product_data))
-    return product_in_db
 
+@database_sync_to_async
+def create_product_in_db(product_data: ProductIn):
+    product = ProductModel.objects.create(**dict(product_data))
+    return product
+
+
+@router.post("/", response_model=Product)
+async def create_product(product_data: ProductIn):
+    product_in_db = await create_product_in_db(product_data)
+    return product_in_db
